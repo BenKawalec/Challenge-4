efd2c897f4bc98dcc38c03feeebbc2cc67128b07
Ben Kawalec <bkawalec10@gmail.com>
2022-08-16 14:15:49 -0400

    Start page and basic question page setup

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
 let startBtn = document.querySelector('#startBtn')
-let timeEl = document.querySelector('#seconds')
-
+let timeEl = document.querySelector('#timer')
+let questionHide = document.querySelector('#question-container')
+let startButton = document.querySelector('#startBtn')
+let startPage = document.querySelector('#start-page')
+let nextBtn = document.querySelector('#next-btn')
 startBtn.addEventListener('click', startGame)
+
+// When page loads it hides the question section
+window.onload = (event) => {
+  questionHide.classList.add('hide')
+  nextBtn.classList.add('hide')
+};
 
 // When start button pressed 
 function startGame() {
   timer()
+  hideOnStart()
+  questionHide.classList.remove('hide')
+  nextBtn.classList.remove('hide')
 }
 
+// Initial Time display
 let timeLeft = 10
 timeEl.textContent = `Time: ${timeLeft}`
-
+// Function to run the timer
 function timer() {
   function time(){ 
     timeLeft--;
+    if (timeLeft < 0) {
+      return
+    }
     timeEl.textContent = `Time: ${timeLeft}`
   }
   timeInterval = setInterval(time, 1000)
+}
+
+// When function called it adds a class with "hide" to the elements
+function hideOnStart() {
+  startPage.classList.add('hide');
+  startButton.classList.add('hide')
 }
